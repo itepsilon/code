ed4e67fec01dc303e07ddd02a80fb3c983cdefe0
itepsilon <ganbold.li@gmail.com>
2019-01-29 16:24:06 -0800

    power of three @leetcode

---
+++ leetcode/power_of_three/power_of_three.cpp
@@ -0,0 +1,8 @@
+class PowerOfThree {
+    public:
+        static bool isPowerOfThree(int n) {
+            if(n <= 0) return false;
+            while(n % 3 == 0) n /= 3;
+            return n == 1;
+        }
+};
