4790c466d475478a195a1e7d3ecfb208fb26218c
itepsilon <ganbold.li@gmail.com>
2019-02-10 20:30:01 -0800

    middle of the linked list

---
+++ leetcode/middle_of_the_linked_list/middle_of_the_linked_list.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  ListNode *middleNode(ListNode *head) {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast != NULL && fast->next != NULL) {
+      fast = fast->next->next;
+      slow = slow->next;
+    }
+    return slow;
+  }
+};
