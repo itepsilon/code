eadc88d25a0c2dfccdc65b77f7cfb4dfda7865b6
itepsilon <ganbold.li@gmail.com>
2019-02-14 20:59:42 -0800

    unique morse code words

---
+++ leetcode/unique_morse_code_words/unique_morse_code_words.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int uniqueMorseRepresentations(vector<string>& words) {
+    vector<string> hashtable {".-",   "-...", "-.-.", "-..",  ".",    "..-.", "--.",
+                     "....", "..",   ".---", "-.-",  ".-..", "--",   "-.",
+                     "---",  ".--.", "--.-", ".-.",  "...",  "-",    "..-",
+                     "...-", ".--",  "-..-", "-.--", "--.."};
+    unordered_set<string> s;
+
+    for (const string& w : words) {
+        string result = "";
+        for (const char& ch : w) {
+            result += hashtable[ch - 'a'];
+        }
+        s.insert(result);
+    }
+    return s.size();
+  }
+};
