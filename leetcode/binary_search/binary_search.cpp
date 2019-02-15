5db9f37bac181de83a6afacb1c4337c6ffcba93a
itepsilon <ganbold.li@gmail.com>
2019-02-14 20:45:07 -0800

    binary search

---
+++ leetcode/binary_search/binary_search.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+using namespace std;
+class Solution {
+ public:
+  int search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low <= high) {
+      int mid = (low + high) / 2;
+      if (nums[mid] < target) {
+        low = mid + 1;
+      } else if (nums[mid] > target) {
+        high = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+};
